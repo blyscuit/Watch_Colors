b08b9a67bad30a7affde4595d458920469f4efc0
Bliss Wetchayanwiwat <pisit@nimblehq.co>
2022-05-04 18:00:42 +0700

    [snap scroll]

--- pages/index.js
+++ pages/index.js
@@ -61,19 +61,20 @@ export default function Home({ colors }) {
   }
 
   return (
-    <Layout>
+    <div>
       <Head>
         <title>Apple Watch Face Colors</title>
         <meta name="description" content="Color Palletes created from Apple Watch Face Colors" />
       </Head>
       {/* Hero Section  */}
 
-      <section className="flex flex-col  space-y-10 mt-12 sm:mt-24 md:mt-32 pb-24">
+      <div style={{height: "100vh", overflow: "auto"}} className="snap-y w-full absolute" >
+      <section className="flex flex-col space-y-10 mt-12 sm:mt-24 md:mt-32">
         {/* Headlines */}
 
         <h2 className="text-3xl text-center leading-normal sm:text-6xl font-thin  ">Face Colors</h2>
 
-      <div className="snap-y ">
+      <div className="snap-y pb-64">
         {colors.map((d) => (
 
     <motion.div
@@ -85,7 +86,7 @@ export default function Home({ colors }) {
   >
       <div className="snap-center py-12" key={d.id}>
         <motion.div variants={textVariants}>
-            <p className="text-xl sm:text-md px-12 ">{d.name}</p>
+            <p className="text-xl sm:text-md px-12 pb-8 ">{d.name}</p>
         </motion.div>
 
             <motion.div variants={cardVariants} className="grid grid-cols-4 sm:grid-cols-6 gap-12 pt-4 pb-8 px-12 overflow-x-auto hover:overflow-x-scroll">
@@ -102,17 +103,19 @@ export default function Home({ colors }) {
         ))}
         </div>
       </section>
-      <ToastContainer transition={Slide} theme="dark" position="bottom-right" />
-      
       <div className={
         (likes.length > 0 ? "translate-y-0 " : "translate-y-96 " )+
       "transition ease-in-out  flex items-center flex-row sticky bottom-0 bg-gray-900 left-0 right-0 px-8 py-2"}>
               <div className="h-8 w-8 rounded-lg" style={{backgroundColor: likes }}></div>
               <button onClick={handleCopy} className="px-6 grow text-left" >{likes}</button>
               <button onClick={handleCopy} className="btn justify-self-end">Copy</button>
-
       </div>
-    </Layout>
+      </div>
+      <ToastContainer transition={Slide} theme="dark" position="bottom-right" />
+      
+
+
+    </div>
 
   );
 }
