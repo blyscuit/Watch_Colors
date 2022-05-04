7e3e247e4ce527cd25f3d190d820e031a19b021f
Bliss Wetchayanwiwat <pisit@nimblehq.co>
2022-05-04 14:01:18 +0700

    [Color]

--- data/data.js
+++ data/data.js
@@ -8,5 +8,20 @@ export const colors = [
         id: "fl2021",
         name: "Fall 2021",
         colors: [ "#706F8C", "#815668", "#5C8649", "#E5C5A4", "#BC122E", "#4A778B" ]
+    },
+    {
+        id: "sm2021",
+        name: "Summer 2021",
+        colors: [ "#FE4C36", "#DCE1DE", "#6D5E3F" ]
+    },
+    {
+        id: "sp2021",
+        name: "Spring 2021",
+        colors: [ "#FFA100", "#C0DFA3", "#40578C", "#F6F1D7", "#4E7378" ]
+    },
+    {
+        id: "fl2021",
+        name: "Fall 2021",
+        colors: [ "#EF4F47", "#5C7299", "#739B9D", "#FE8851", "#FFE3B7", "#636D59", "#93424D" ]
     }
 ]
--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 export default function Custom404() {
-    return null;
+    return (<></>);
   }
   
   export const getStaticProps = () => {
