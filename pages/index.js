14f6f8ee28ac3d9ee1c1ce61b7d880c567de498f
Bliss Wetchayanwiwat <pisit@nimblehq.co>
2022-05-04 17:28:33 +0700

    [animation]

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Layout from "../sections/Layout";
 import { colors } from "../data/data";
 import Head from 'next/head'
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Slide } from 'react-toastify';
+import { motion } from 'framer-motion';
 
 export const getStaticProps = async () => {
   return {
@@ -11,6 +16,50 @@ export const getStaticProps = async () => {
 }
 
 export default function Home({ colors }) {
+  const notify = () => toast("Copied "+likes, { autoClose: 2000, className:"bg-gray-900" });
+  
+  const [likes, setLikes] = React.useState("");
+  
+
+  const cardVariants = {
+    offscreen: {
+      opacity: 0.2,
+      scale: 0.9
+    },
+    onscreen: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
+
+  const textVariants = {
+    offscreen: {
+      opacity: 0.2,
+    },
+    onscreen: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
+
+  function handleClick(color) {
+    setLikes(color)
+  }
+
+  function handleCopy() {
+    navigator.clipboard.writeText(likes)
+    notify()
+  }
+
   return (
     <Layout>
       <Head>
@@ -21,22 +70,49 @@ export default function Home({ colors }) {
 
       <section className="flex flex-col  space-y-10 mt-12 sm:mt-24 md:mt-32 pb-24">
         {/* Headlines */}
-        
+
         <h2 className="text-3xl text-center leading-normal sm:text-6xl font-thin  ">Face Colors</h2>
 
+      <div className="snap-y ">
         {colors.map((d) => (
-          <div key={d.id}>
-            <p className="text-xl sm:text-md px-12 ">{d.name}</p>
-            
-            <div className="grid grid-cols-6 gap-12 pt-4 pb-8 px-12 overflow-x-auto hover:overflow-x-scroll">
-            {d.colors.map((color) => (
-                <div key={color} style={{backgroundColor: color }} className="rounded-full h-16 w-16"></div>
-            ))}
-              </div>
 
-          </div>
+    <motion.div
+    className="card-container"
+    initial="offscreen"
+    whileInView="onscreen"
+    margin="-200px 0px -200px 0px"
+    viewport={{ once: false, amount: 1.0 }}
+  >
+      <div className="snap-center py-12" key={d.id}>
+        <motion.div variants={textVariants}>
+            <p className="text-xl sm:text-md px-12 ">{d.name}</p>
+        </motion.div>
+
+            <motion.div variants={cardVariants} className="grid grid-cols-4 sm:grid-cols-6 gap-12 pt-4 pb-8 px-12 overflow-x-auto hover:overflow-x-scroll">
+            {d.colors.map((color) => (
+              color == likes ?
+                <button onClick={() => handleClick(color)} key={color} style={{borderColor: color, backgroundColor: "transparent" }} className="rounded-full h-16 w-16 border border-8"></button>
+                :
+                <button onClick={() => handleClick(color)} key={color} style={{backgroundColor: color }} className="rounded-full h-16 w-16"></button>
+            ))}
+            </motion.div>
+
+      </div>
+    </motion.div>
         ))}
+        </div>
       </section>
+      <ToastContainer transition={Slide} theme="dark" position="bottom-right" />
+      
+      <div className={
+        (likes.length > 0 ? "translate-y-0 " : "translate-y-96 " )+
+      "transition ease-in-out  flex items-center flex-row sticky bottom-0 bg-gray-900 left-0 right-0 px-8 py-2"}>
+              <div className="h-8 w-8 rounded-lg" style={{backgroundColor: likes }}></div>
+              <button onClick={handleCopy} className="px-6 grow text-left" >{likes}</button>
+              <button onClick={handleCopy} className="btn justify-self-end">Copy</button>
+
+      </div>
     </Layout>
+
   );
 }
