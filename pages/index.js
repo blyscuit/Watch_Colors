f51c2757d7fa0f0b6299389e8fd051c9e461f602
Bliss Wetchayanwiwat <pisit@nimblehq.co>
2022-05-05 10:13:38 +0700

    [scroll] fix ios scroll

--- pages/index.js
+++ pages/index.js
@@ -68,29 +68,29 @@ export default function Home({ colors }) {
       </Head>
       {/* Hero Section  */}
 
-      <div style={{height: "100vh", overflow: "auto"}} className="snap-y w-full absolute" >
+      <div style={{height: "100vh", overflowY: "auto"}} className="snap-y w-full absolute overflow-x-hidden " >
       <section className="flex flex-col space-y-10 mt-12 sm:mt-24 md:mt-32">
         {/* Headlines */}
 
         <h2 className="text-3xl text-center leading-normal sm:text-6xl font-thin  ">Face Colors</h2>
 
-      <div className="snap-y pb-64">
+      <div className="snap-y pb-64 ">
         {colors.map((d) => (
 
     <motion.div
     key={d}
-    className="card-container"
+    className="card-container "
     initial="offscreen"
     whileInView="onscreen"
     margin="-200px 0px -200px 0px"
-    viewport={{ once: false, amount: 1.0 }}
+    viewport={{ once: false, amount: 0.8 }}
   >
       <div className="snap-center py-12" key={d.id}>
         <motion.div variants={textVariants}>
-            <p className="text-xl sm:text-md px-12 pb-8 ">{d.name}</p>
+            <p className="text-xl sm:text-md px-4 sm:px-12 pb-8 ">{d.name}</p>
         </motion.div>
 
-            <motion.div variants={cardVariants} className="grid grid-cols-4 sm:grid-cols-6 gap-12 pt-4 pb-8 px-12 overflow-x-auto hover:overflow-x-scroll">
+            <motion.div variants={cardVariants} className="grid grid-cols-4 sm:grid-cols-6 gap-12 pt-4 pb-8 px-4 sm:px-12 overflow-x-auto hover:overflow-x-scroll">
             {d.colors.map((color) => (
               color == likes ?
                 <button onClick={() => handleClick(color)} key={color} style={{borderColor: color, backgroundColor: "transparent" }} className="rounded-full h-16 w-16 border border-8"></button>
@@ -104,13 +104,15 @@ export default function Home({ colors }) {
         ))}
         </div>
       </section>
+        {likes.length > 0 ?
       <div className={
+        (likes.length > 0 ? "visible " : "invisible " )+
         (likes.length > 0 ? "translate-y-0 " : "translate-y-96 " )+
       "transition ease-in-out  flex items-center flex-row sticky bottom-0 bg-gray-900 left-0 right-0 px-8 py-2"}>
               <div className="h-8 w-8 rounded-lg" style={{backgroundColor: likes }}></div>
               <button onClick={handleCopy} className="px-6 grow text-left" >{likes}</button>
               <button onClick={handleCopy} className="btn justify-self-end">Copy</button>
-      </div>
+      </div>:<div/>}
       </div>
       <ToastContainer transition={Slide} theme="dark" position="bottom-right" />
       
